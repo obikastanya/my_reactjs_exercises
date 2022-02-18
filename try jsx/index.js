5f185720c3a4772e1d6915f3b981d92596462cc4
obikastanya <obikastanya@gmail.com>
2022-02-18 22:55:32 +0700

    use function in jsx

--- try jsx/index.js
+++ try jsx/index.js
@@ -1,5 +1,13 @@
-const name = 'Josh Perez'
-const element = <h1>Hello, {name}</h1>
+function formatName( user ) {
+    return user.firstName + ' ' + user.lastName
+}
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+}
+const element = (
+    <h1>Hello, {formatName( user )}</h1>
+)
 ReactDOM.render(
     element,
     document.getElementById( 'root' )

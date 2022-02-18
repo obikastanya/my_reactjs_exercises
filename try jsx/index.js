7b42e43eaf81e39d241f4b1869dbd9e668c327dd
obikastanya <obikastanya@gmail.com>
2022-02-18 23:13:55 +0700

    replace elements

--- try jsx/index.js
+++ try jsx/index.js
@@ -27,3 +27,16 @@ ReactDOM.render(
 // const element = <img src={user.avatarUrl}></img>
 
 // jsx mengubah semua value menjadi string sebelum di render, sehingga tidak memungkinkan scripting dapat berjalan
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    )
+    ReactDOM.render(
+        element,
+        document.getElementById( 'root' ) )
+}
+setInterval( tick, 1000 )

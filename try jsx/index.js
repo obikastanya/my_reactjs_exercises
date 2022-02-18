05fe21addd2809f82821707fcb15c03a79d63ab1
obikastanya <obikastanya@gmail.com>
2022-02-18 23:09:22 +0700

    intro to jsx

--- try jsx/index.js
+++ try jsx/index.js
@@ -12,3 +12,18 @@ ReactDOM.render(
     element,
     document.getElementById( 'root' )
 )
+
+// use jsx as expression
+// function getGreeting( user ) {
+//     if ( user ) {
+//         return <h1>Hello, {formatName( user )} !</h1>
+//     }
+//     return <h1>Hello, Stranger</h1>
+// }
+
+// Specifying attributes with jsx
+// const element = <a href='https://reactjs.com'></a>
+// or you could use like this, dont use quote
+// const element = <img src={user.avatarUrl}></img>
+
+// jsx mengubah semua value menjadi string sebelum di render, sehingga tidak memungkinkan scripting dapat berjalan

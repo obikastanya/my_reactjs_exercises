9d095d1af5efed2d09ae4ba6069bbdf751dc0e13
obikastanya <obikastanya@gmail.com>
2022-02-19 00:09:31 +0700

    try state

--- try jsx/state.js
+++ try jsx/state.js
@@ -14,19 +14,25 @@
 
 // Make it more encapsulated
 
-function Clock( props ) {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-            <h2>It is
-                {props.date.toLocaleTimeString()}.
-            </h2>
-        </div>
-    )
+class Clock extends React.Component {
+    constructor( props ) {
+        super( props )
+        this.state = { date: new Date() }
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.
+                </h2>
+            </div >
+        )
+    }
 }
 function tick() {
     ReactDOM.render(
-        <Clock date={new Date()} />,
+        <Clock />,
         document.getElementById( 'root' )
     )
 }

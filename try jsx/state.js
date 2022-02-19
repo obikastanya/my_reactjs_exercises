81aa17b9296afa007e10de3c6446aae16f3565a5
obikastanya <obikastanya@gmail.com>
2022-02-19 11:50:45 +0700

    3 things to remember about state

--- try jsx/state.js
+++ try jsx/state.js
@@ -52,8 +52,59 @@ function tick() {
     )
 }
 setInterval( tick, 1000 )
+    // state adalah object yang yang menyimpan data pada react
+    // jika kita ingin melakukan sesuatu saat element pertama kali
+    //  dibuat atau saat dom dihapus kita dapat menambahkan fungsi pada class, yaitu mount dan unmounts.
+    // method tersebut disebut lifecycle methods.
 
-// jika kita ingin melakukan sesuatu saat element pertama kali
-//  dibuat atau saat dom dihapus kita dapat menambahkan fungsi pada class, yaitu mount dan unmounts.
-// method tersebut disebut lifecycle methods.
+    // Ada 3 hal yang harus diingat tentang state
+    // 1. Jangan mengubah state secara langsung, assign state hanya dilakukan di constructor
+    // this.state.comment = 'hello'
+    // tapi gunakanlah setState
+    // this.setState( { comment: 'Hello' } )
+    // set state or probs is asynchronous . so, you cant relying them.
+    // To fix this, use probs or state that accept function instead of object
+    // wrong sample:
+    // this.setState( {
+    //     counter: this.state.counter + this.props.increment
+    // } )
+    // instead, you could use this
+    // this.setState( ( state, props ) => {
+    //     counter: state.counter + props.increment
+    // } )
 
+
+    // set stage is merged. When you have 2 different state independently,
+    // you can update 2 of them separately. react would merge them, not replace them.
+
+    // constructor( props ){
+    //     super( props )
+    //     this.state = {
+    //         posts: [],
+    //         comments: []
+    //     }
+
+    // }
+    // componentDidMount(){
+    //     fetchPost().then( response => {
+    //         this.setState(
+    //             {
+    //                 posts: response.posts
+    //             }
+    //         )
+    //     } )
+    //     fetchComments().then( response => {
+    //         this.setState( {
+    //             comments: response.comments
+    //         } )
+    //     } )
+    // }
+
+
+    // props bersifat encapsulated, dimana hanya dapat diakses oleh component yang memilikinya
+    // Jika child component ingin menggunakan props, props harus di teruskan ke child component
+
+    // < FotmattedDate date = { this.state.date } />
+    //     function FormattedDate( props ) {
+    //         return <h2>It is {props.date.toLocalTimeString()}.</h2>
+    //     }
